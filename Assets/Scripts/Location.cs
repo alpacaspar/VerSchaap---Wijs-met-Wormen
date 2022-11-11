08963a2a24ec3204a7c760d3f65bc8f4b9841332
CasparG <caspar.gelderman@gmail.com>
2022-11-11 15:16:21 +0100

    Weather API implemented

---
+++ Assets/Scripts/Location.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Location : MonoBehaviour
+{
+    private string ipAddress;
+    public LocationInfo info;
+    private float latitude;
+    private float longitude;
+    private string timezone;
+    
+    public WeatherInfo weatherInfo;
+    
+    private void Start()
+    {
+        StartCoroutine(GetIPAddress());
+    }
+    
+    private IEnumerator GetIPAddress()
+    {
+        var www = UnityWebRequest.Get("http://checkip.dyndns.org");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+            yield break;
+        }
+
+        var result = www.downloadHandler.text;
+
+        var a = result.Split(':');
+        var a2 = a[1].Substring(1);
+        var a3 = a2.Split('<');
+        var a4 = a3[0];
+
+        ipAddress = a4;
+        
+        StartCoroutine(GetCoordinates());
+    }
+
+    private IEnumerator GetCoordinates()
+    {
+        var www = new UnityWebRequest("http://ip-api.com/json/" + ipAddress)
+        {
+            downloadHandler = new DownloadHandlerBuffer()
+        };
+
+        yield return www.SendWebRequest();
+        
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+            yield break;
+        }
+
+        info = JsonUtility.FromJson<LocationInfo>(www.downloadHandler.text);
+        latitude = info.lat;
+        longitude = info.lon;
+        timezone = info.timezone;
+        
+        StartCoroutine(GetWeather());
+    }
+    
+    // This is a copy of the function in Weather.cs, so this will be gone after the event system has been put in place
+    private IEnumerator GetWeather()
+    {
+        var www = new UnityWebRequest($"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,relativehumidity_2m,precipitation,cloudcover&timezone={timezone}")
+        {
+            downloadHandler = new DownloadHandlerBuffer()
+        };
+
+        yield return www.SendWebRequest();
+        
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+            yield break;
+        }
+
+        weatherInfo = JsonUtility.FromJson<WeatherInfo>(www.downloadHandler.text);
+    }
+}
+
+[Serializable]
+public class LocationInfo
+{
+    public string query;
+    public string status;
+    public string continent;
+    public string continentCode;
+    public string country;
+    public string countryCode;
+    public string region;
+    public string regionName;
+    public string city;
+    public string district;
+    public string zip;
+    public float lat;
+    public float lon;
+    public string timezone;
+    public string offset;
+    public string currency;
+    public string isp;
+    public string org;
+    public string @as;
+    public string asname;
+    public bool mobile;
+    public bool proxy;
+    public bool hosting;
+}
---
+++ Assets/Scripts/Weather.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Weather : MonoBehaviour
+{
+    public WeatherInfo info;
+    private float latitude;
+    private float longitude;
+    private string timezone;
+
+    // documentation for open-meteo api: https://open-meteo.com/en/docs
+    private void Start()
+    {
+        StartCoroutine(GetWeather());
+    }
+
+    private IEnumerator GetWeather()
+    {
+        var www = new UnityWebRequest($"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,relativehumidity_2m,precipitation,cloudcover&timezone={timezone}")
+        {
+            downloadHandler = new DownloadHandlerBuffer()
+        };
+
+        yield return www.SendWebRequest();
+        
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+            yield break;
+        }
+
+        info = JsonUtility.FromJson<WeatherInfo>(www.downloadHandler.text);
+    }
+}
+
+[Serializable]
+public class WeatherInfo
+{
+    public float latitude;
+    public float longitude;
+    public float generationtime_ms;
+    public int utc_offset_seconds;
+    public string timezone;
+    public string timezone_abbreviation;
+    public int elevation;
+    public HourlyUnits hourly_units;
+    public Hourly hourly;
+}
+
+[Serializable]
+public class Hourly
+{
+    public string[] time;
+    public float[] temperature_2m;
+    public int[] relativehumidity_2m;
+    public int[] precipitation;
+    public int[] cloudcover;
+}
+
+[Serializable]
+public class HourlyUnits
+{
+    public string time;
+    public string temperature_2m;
+    public string relativehumidity_2m;
+    public string precipitation;
+    public string cloudcover;
+}
