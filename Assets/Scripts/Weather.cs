f94156f4e7b824d35040fc3e7bfc7d87f60aec76
CasparG00 <61011805+CasparG00@users.noreply.github.com>
2022-11-14 13:26:05 +0100

    Update Weather.cs

--- Assets/Scripts/Weather.cs
+++ Assets/Scripts/Weather.cs
@@ -13,10 +13,19 @@ public class Weather : MonoBehaviour
     // documentation for open-meteo api: https://open-meteo.com/en/docs
     private void Start()
     {
-        StartCoroutine(GetWeather());
+        UpdateWeather();
     }
 
-    private IEnumerator GetWeather()
+    
+    /// <summary>
+    /// Update weather information.
+    /// </summary>
+    public void UpdateWeather()
+    {
+        StartCoroutine(GetWeather(latitude, longitude, timezone));
+    }
+    
+    private IEnumerator GetWeather(float latitude, float longitude, string timezone)
     {
         var www = new UnityWebRequest($"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,relativehumidity_2m,precipitation,cloudcover&timezone={timezone}")
         {
@@ -32,6 +41,15 @@ public class Weather : MonoBehaviour
         }
 
         info = JsonUtility.FromJson<WeatherInfo>(www.downloadHandler.text);
+    }
+
+    /// <summary>
+    /// Returns the current weather information.
+    /// </summary>
+    /// <returns></returns>
+    public WeatherInfo GetWeatherInfo()
+    {
+        return info;
     }
 }
 
