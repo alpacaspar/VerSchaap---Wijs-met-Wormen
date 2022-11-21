3e66c8ef44a44ddc38479ccc727f85823dac6274
CasparG00 <61011805+CasparG00@users.noreply.github.com>
2022-11-21 16:27:35 +0100

    Implemented avg. temperature and day of the week

--- Assets/Scripts/UI/WeeklyForecast.cs
+++ Assets/Scripts/UI/WeeklyForecast.cs
@@ -1,10 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class WeeklyForecast : MonoBehaviour
 {
     [SerializeField] private GameObject dailyForecastPrefab;
     [SerializeField] private WeatherInfo weatherInfo;
-    
+
+    [SerializeField] private WeatherIcon[] icons;
+
+    private Dictionary<WeatherType, Sprite> WeatherIcons
+    {
+        get => icons.ToDictionary(x => x.weatherType, x => x.icon);
+    }
 
     private void OnEnable()
     {
@@ -20,10 +29,58 @@ public class WeeklyForecast : MonoBehaviour
     {
         weatherInfo = info;
 
-        for (var day = 0; day < 7; day++)
+        var day = 0;
+        for (var i = 0; i < weatherInfo.hourly.time.Length; i++)
         {
-            var instance = Instantiate(dailyForecastPrefab, transform);
-            instance.GetComponent<DailyForecast>().SetDailyForecastData("", null, 2, 2);
+            if (i % 24 == 0)
+            {
+                day++;
+
+                var forecastDay = ((int)DateTime.Today.DayOfWeek + day - 1) % 7;
+                var dayAbb = GetDayAbb((DayOfWeek)forecastDay);
+                
+                var instance = Instantiate(dailyForecastPrefab, transform);
+                instance.GetComponent<DailyForecast>().SetDailyForecastData(dayAbb, null, GetAvgTemp(info, i, 5, 18), GetAvgTemp(info, i, 19, 24));
+            }
         }
+    }
+    
+    private int GetAvgTemp(WeatherInfo info, int startIndex, int startTime, int endTime)
+    {
+        var temp = 0f;
+
+        for (var i = startIndex + startTime; i < startIndex + endTime; i++)
+        {
+            temp += info.hourly.temperature_2m[i];
+        }
+
+        return Mathf.RoundToInt(temp / (endTime - startTime));
+    }
+
+    /// <summary>
+    /// Converts DayOfWeek enum to dutch abbreviation.
+    /// </summary>
+    /// <param name="dayOfWeek">DayOfTheWeek enum.</param>
+    /// <returns>Returns dutch abbreviation of the DayOfWeek input as string.</returns>
+    public static string GetDayAbb(DayOfWeek dayOfWeek)
+    {
+        return dayOfWeek switch
+        {
+            DayOfWeek.Monday => "Ma",
+            DayOfWeek.Tuesday => "Di",
+            DayOfWeek.Wednesday => "Wo",
+            DayOfWeek.Thursday => "Do",
+            DayOfWeek.Friday => "Vr",
+            DayOfWeek.Saturday => "Za",
+            DayOfWeek.Sunday => "Zo",
+            _ => null
+        };
+    }
+
+    [Serializable]
+    private class WeatherIcon
+    {
+        public WeatherType weatherType;
+        public Sprite icon;
     }
 }
