92b7ef59404f6cd389881b9009c70b9e40e4e78a
CasparG00 <61011805+CasparG00@users.noreply.github.com>
2022-11-29 14:01:56 +0100

    added class for getting local IP

--- Assets/Scripts/NetworkTest.cs
+++ Assets/Scripts/NetworkTest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Net;
+using System.Net.Sockets;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -14,5 +16,18 @@ public class NetworkTest : MonoBehaviour
         {
             transform.AddComponent<HttpClientTest>();
         }
+    }
+    
+    public static string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        throw new Exception("No network adapters with an IPv4 address in the system!");
     }
 }
