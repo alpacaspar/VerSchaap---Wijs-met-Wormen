965835385c744517222c683ddfc41b1d549f45ba
Ravi Bechoe <ravibechoe@outlook.com>
2023-01-09 15:38:51 +0100

    Update VerweidklokPostRequests.php

--- Assets/Scripts/PHP/VerweidklokPostRequests.php
+++ Assets/Scripts/PHP/VerweidklokPostRequests.php
@@ -6,10 +6,12 @@
 	    $result = $mysqli->query("INSERT INTO `VerweidklokSheepTable`(Sheep_UUID, Sheep_Label, Sheep_Male, Farmer_UUID)"
 			."VALUES (".$_GET['Sheep_UUID'].",".$_GET['Sheep_Label'].",".$_GET['Sheep_Male'].",".$_GET['Farmer_UUID'].");");
 
-	    while($row = mysqli_fetch_assoc($result))
+	    echo($result);
+
+	    /*while($row = mysqli_fetch_assoc($result))
 	    {
 	        echo($row);
-	    }
+	    }*/
 	}
 	else
 	{
