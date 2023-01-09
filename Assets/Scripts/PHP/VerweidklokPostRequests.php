60646a2430462bf52f25be983ed621eb39dc7faf
Ravi Bechoe <ravibechoe@outlook.com>
2023-01-09 16:18:19 +0100

    Update VerweidklokPostRequests.php

--- Assets/Scripts/PHP/VerweidklokPostRequests.php
+++ Assets/Scripts/PHP/VerweidklokPostRequests.php
@@ -3,10 +3,10 @@
 
 	if ($_GET['request'] == 'AddSheep')
 	{
-	    $result = $mysqli->query("INSERT INTO `VerweidklokSheepTable` ('Sheep_UUID', 'Sheep_Label', 'Sheep_Female', 'Farmer_UUID')"
+	    $result = $mysqli->query("INSERT INTO `VerweidklokSheepTable` (Sheep_UUID, Sheep_Label, Sheep_Female, Farmer_UUID)"
 			." VALUES ('".$_GET["Sheep_UUID"]."','".$_GET["Sheep_Label"]."','".$_GET["Sheep_Female"]."','".$_GET["Farmer_UUID"]."');");
 
-	    echo("INSERT INTO `VerweidklokSheepTable` ('Sheep_UUID', 'Sheep_Label', 'Sheep_Female', 'Farmer_UUID')"
+	    echo("INSERT INTO `VerweidklokSheepTable` (Sheep_UUID, Sheep_Label, Sheep_Female, Farmer_UUID)"
 			." VALUES ('".$_GET["Sheep_UUID"]."','".$_GET["Sheep_Label"]."','".$_GET["Sheep_Female"]."','".$_GET["Farmer_UUID"]."');");
 
 	    while($row = mysqli_fetch_assoc($result))
