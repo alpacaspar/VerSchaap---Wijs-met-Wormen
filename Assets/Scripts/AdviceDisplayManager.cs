2ca2c2de22a1aa362d0549f920bf4bc63da6de7d
CasparG00 <61011805+CasparG00@users.noreply.github.com>
2023-01-09 15:15:32 +0100

    Started work on more settings

--- Assets/Scripts/AdviceDisplayManager.cs
+++ Assets/Scripts/AdviceDisplayManager.cs
@@ -1,14 +1,29 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AdviceDisplayManager : MonoBehaviour
 {
-    private void Awake()
+    private List<WeideObject> weideObjects = new();
+    
+    private void Start()
     {
         CronScheduler.instance.SetRepeat(this,"UpdateAdviceDisplays", 3600);
     }
 
+    // OLD
     private void UpdateAdviceDisplays()
     {
         EventSystem.InvokeEvent(EventType.performAdviceUpdate);
+    }
+
+    private void GetWeideObjects()
+    {
+        weideObjects.Clear();
+        
+        var results = WurmAPI.MethodHandler<WeideObject>(MethodType.Get, null);
+        foreach (var result in results)
+        {
+            weideObjects.Add(JsonUtility.FromJson<WeideObject>(result));
+        }
     }
 }
