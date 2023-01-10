ce267bd2dd5a18dfb445eed3e2aaa95aa917a07f
Ravi Bechoe <ravibechoe@outlook.com>
2023-01-10 15:16:19 +0100

    crying

--- Assets/Scripts/PHP/VerweidklokGetRequests.php
+++ Assets/Scripts/PHP/VerweidklokGetRequests.php
@@ -5,8 +5,8 @@
 	{
 		if (isset($_GET["Sheep_UUID"]) and isset($_GET["Farmer_UUID"]))
 		{
-		    $result = $mysqli->query("SELECT * FROM `VerweidklokSheepTable` WHERE Sheep_UUID = '"
-		    	.$_GET(Sheep_UUID)."' AND Farmer_UUID = '".$_GET(Farmer_UUID)."';");
+		    $result = $mysqli->query("SELECT * FROM `VerweidklokSheepTable` WHERE Sheep_UUID = " 
+		    	."'".$_GET[Sheep_UUID]."' AND Farmer_UUID = '".$_GET[Farmer_UUID]."';");
 
 		    while($row = mysqli_fetch_assoc($result))
 		    {
